1d88e63c384c5a3f7b74b086ae4c09cd6308f23e
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-17 23:29:21 +0900

    20221017change

---
+++ day01/src/ch02/UserId.java
@@ -0,0 +1,12 @@
+package ch02;
+
+public class UserId {
+    public static void main(java.lang.String[] args) {
+
+        java.lang.String userId;
+        java.lang.String userPassword;
+        java.lang.String userName;
+        java.lang.String address;
+        int phoneNumber;
+    }
+}
